b21c4d8fac3892ad6f86d395c6e13cc8d6bd784d
Karan Bhosale <karanbhosale356@gmail.com>
2026-04-16 19:52:27 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+import json
+
+def save_results(optimizer_name, accuracy):
+    data = {
+        "optimizer": optimizer_name,
+        "accuracy": float(accuracy)
+    }
+
+    with open("results.json", "a") as f:
+        f.write(json.dumps(data) + "\n")
